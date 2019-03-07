3c372dad4bc343c3447d8ff1cb623e1bd9b4a11f
Thom <Thomdemeulder@gmail.com>
2019-03-07 15:42:01 +0100

    add random apple rotation and collision fix

--- Assets/Scripts/AppleBehaviour/AppleController.cs
+++ Assets/Scripts/AppleBehaviour/AppleController.cs
@@ -10,11 +10,29 @@ public class AppleController : MonoBehaviour, IControllerTarget
     [System.NonSerialized]
     public GenericPool originalPool;
 
+    protected bool xNegative, yNegative, zNegative;
+
+    private void Awake()
+    {
+        xNegative = ShouldBeNegative();
+        yNegative = ShouldBeNegative();
+        zNegative = ShouldBeNegative();
+        Debug.Log($"Values {xNegative}, {yNegative}, {zNegative}");
+    }
+
     protected virtual void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Ground")) return;
         originalPool.ReturnObject(gameObject);
     }
+
+    protected virtual bool ShouldBeNegative() => Random.Range(0, 2) == 0;
+
+    protected virtual void Update() => transform.Rotate(GetRandomRotation(xNegative), GetRandomRotation(yNegative), GetRandomRotation(zNegative));
+
+    protected virtual float GetRandomRotation(bool negative) => negative ? -GetRandomRotation() : GetRandomRotation();
+
+    protected virtual float GetRandomRotation() => Random.Range(5, 10) * Time.deltaTime;
 
     public virtual void OnGrab()
     {
